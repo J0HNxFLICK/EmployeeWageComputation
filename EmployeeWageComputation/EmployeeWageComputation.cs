312d853edeac72825fb49175bf12a463a027d5e8
J0HNxFLICK <nirmal.arun.ashish@gmail.com>
2022-12-20 09:19:55 +0530

    [Arun] Added employee attendance check

--- EmployeeWageComputation/EmployeeWageComputation.cs
+++ EmployeeWageComputation/EmployeeWageComputation.cs
@@ -5,6 +5,19 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program!");
+            const int present = 1;
+
+            //UC1 Check employee is present or absent using RANDOM
+            Random random = new Random();
+            int fullTimeEmployee = random.Next(0, 2);
+            if (fullTimeEmployee == present)
+            {
+                Console.WriteLine("The employee is present.");
+            }
+            else
+            {
+                Console.WriteLine("The Employee is absent.");
+            }
         }
     }
 }
