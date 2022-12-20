98f0b1ad32f2d9d7f645af6ffed3867f1b14c4e7
J0HNxFLICK <nirmal.arun.ashish@gmail.com>
2022-12-20 09:29:20 +0530

    [Arun] Added Daily employee wage calculation

--- EmployeeWageComputation/EmployeeWageComputation.cs
+++ EmployeeWageComputation/EmployeeWageComputation.cs
@@ -6,6 +6,8 @@
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program!");
             const int present = 1;
+            const int wagePerHr = 20;
+            int fullDayHrs = 0;
 
             //UC1 Check employee is present or absent using RANDOM
             Random random = new Random();
@@ -13,11 +15,22 @@
             if (fullTimeEmployee == present)
             {
                 Console.WriteLine("The employee is present.");
+
+                fullDayHrs += 8;
             }
             else
             {
                 Console.WriteLine("The Employee is absent.");
+
+                fullDayHrs += 0;
             }
+
+            //UC2 Calculate daily employee wage
+            int fullTimeWage = wagePerHr * fullDayHrs;
+            Console.WriteLine("Total wage is : " + fullTimeWage);
+
+
+
         }
     }
 }
